de1a0687ba1b5ca9953297b22b8b5a0622c29939
san <muhammad.ikhsan2946@student.unri.ac.id>
2022-11-27 10:33:16 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getAllNotesByIdHandler,
+  editNoteByIdHandler,
+  deleteNotebyIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getAllNotesByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNotebyIdHandler,
+  },
+];
+
+module.exports = routes;
